98fd9eee60ff0c8cb219f07e7c9c91876d173c4f
Kevin Han <yd.han181@gmail.com>
2020-01-31 13:13:23 -0500

    Set up TypeScript. Added react-scripts as a dependency. Created public folder.

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+    return (
+        <>
+            <div>Hello world</div>
+        </>
+    )
+}
+
+render(<App></App>, document.getElementById('app'))
