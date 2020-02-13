a86cadce7b1df981216b499c60ed8c0ddde52cc3
Kevin Han <yd.han181@gmail.com>
2020-02-13 15:52:12 -0500

    Updated documentation for utils/scripts/write-file.js. Updated import statement for React in utils/scripts/write-html.ts.

--- utils/scripts/write-file.js
+++ utils/scripts/write-file.js
@@ -1,10 +1,9 @@
 const fs = require('fs').promises
 
 /**
- * Reads a specified template file, uses
- *
- * @param {Object} config - Object literal containing configurations for the
- *                          function
+ * Reads a specified template file, modifies the data within the template file
+ * using the replacement rules, and writes a new file with modifications to the
+ * specified destination path. This function does not mutate the template file.
  *
  * @param {string} config.entry - Entry file path of file template
  * @param {string} config.output - Destination path of output file
@@ -22,6 +21,12 @@ module.exports = async config => {
 
     let file = await fs.readFile(entry, encoding)
 
+    /**
+     * @example
+     * const replacementRules = [
+     *
+     * ]
+     */
     replacementRules.forEach(rule => {
         file = file.replace(rule.searchValue, rule.replacementValue)
     })
--- utils/scripts/write-html.ts
+++ utils/scripts/write-html.ts
@@ -1,11 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 import { renderToString } from 'react-dom/server'
 import App from '../../src/App'
 import writeFile from './write-file'
 import pathConfig from '../../configs/write-html.config'
 
 const writeHtml = async (): Promise<void> => {
-    const html = renderToString(React.createElement(App))
+    const html: string = renderToString(React.createElement(App))
 
     const config = Object.assign(
         {
