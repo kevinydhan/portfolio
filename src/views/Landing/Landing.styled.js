228062915bf4e062a416b22f7e47e975303d3a79
Kevin Han <kevinhanyd@gmail.com>
2020-03-13 22:11:21 -0400

    Updated CircleCI config and package.json scripts to use yarn. Fixed StyleLint errors.

--- src/views/Landing/Landing.styled.js
+++ src/views/Landing/Landing.styled.js
@@ -6,8 +6,8 @@ const { mediaQueries } = theme
 export const Root = styled('section')`
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
+    justify-content: center;
 
     width: 100vw;
     min-height: 100vh;
