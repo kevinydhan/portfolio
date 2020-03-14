f2cc5999a4482b441c06ea48078dedf8af5c00fc
Kevin Han <kevinhanyd@gmail.com>
2020-03-14 10:04:31 -0400

    Added comments to theme variables.

--- src/theme/variables/dimensions.js
+++ src/theme/variables/dimensions.js
@@ -1,4 +1,7 @@
 const dimensions = {
+    /**
+     * Contains the numeric values for the number of pixels for each breakpoint.
+     */
     breakpoints: {
         xs: 0,
         sm: 480,
@@ -6,6 +9,11 @@ const dimensions = {
         lg: 992,
         xl: 1200,
     },
+    /**
+     * Contains CSS dimensional values for heights of components. The purpose
+     * is to utilize the heights to handle scroll events in different parts of
+     * the application.
+     */
     heights: {
         Navbar: '68px',
     },
