c485d7ae1cc50d1697a7557e07517d11776ba07c
Kevin Han <kevinhanyd@gmail.com>
2020-12-01 17:50:23 -0500

    chore: remove v4 code

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Head } from './Head'
-export { default as TopNav } from './TopNav/TopNav'
-export { default as LandingSection } from './LandingSection/LandingSection'
-export { default as ProjectCard } from './ProjectCard/ProjectCard'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,5 @@
 import React from 'react'
-import { Head, TopNav, LandingSection, ProjectCard } from '../components'
-import { useProjectDataQuery } from '../utils'
-import '../styles/global.scss'
-import sharedStyles from '../styles/modules/index.module.scss'
 
-const IndexPage = (): JSX.Element => {
-  const projects = useProjectDataQuery()
-
-  return (
-    <>
-      <Head />
-      <div className={`${sharedStyles.grid} ${sharedStyles.backgroundGrid}`}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <TopNav />
-      <main className={sharedStyles.grid}>
-        <LandingSection />
-        <section id="projects" className={sharedStyles.gridChild}>
-          <h2 className={sharedStyles.projectSectionHeading}>Latest Works</h2>
-          {projects.map((project) => (
-            <ProjectCard key={project.imgDetails.originalName} {...project} />
-          ))}
-        </section>
-      </main>
-    </>
-  )
-}
+const IndexPage = (): JSX.Element => <></>
 
 export default IndexPage
