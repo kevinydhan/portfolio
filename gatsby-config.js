7e415d29c3aa3ffd557893cabd039e36ba141071
Kevin Han <kevinhanyd@gmail.com>
2020-12-01 18:11:46 -0500

    build: update Gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,28 @@ require('dotenv').config()
 module.exports = {
   plugins: [
     {
+      resolve: 'gatsby-plugin-ts-config',
+    },
+    {
       resolve: 'gatsby-plugin-sharp',
     },
     {
       resolve: 'gatsby-transformer-sharp',
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        environment: process.env.CONTENTFUL_ENVIRONMENT,
+        downloadLocal: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-helmet',
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
     },
   ],
 }
